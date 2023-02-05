f6c0256e5c27291f3a4508260c47163c91750560
Nam Nguyen <nam.nt1@teko.vn>
2023-02-05 11:28:50 +0700

    Setup deployment for firebase hosting

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,15 +2,14 @@ import UserService from '../../services/UserService';
 import AuthService from '../../services/AuthService';
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithEmailAndPassword, signInWithPopup } from '@firebase/auth';
 import { googleProvider } from '../../services/FirebaseService';
 
 function Login() {
     const router = useRouter();
 
     const loginWithGoogle = () => {
-        const auth = getAuth();
-        signInWithPopup(auth, googleProvider).then(userCredential => {
+        signInWithPopup(getAuth(), googleProvider).then(userCredential => {
             const googleUser = userCredential.user;
             AuthService.saveToken(googleUser.accessToken);
             UserService.saveGoogleUser(googleUser.uid, googleUser.displayName, googleUser.email, googleUser.photoURL)
@@ -30,14 +29,21 @@ function Login() {
         })
     }
 
+    const loginWithEmailPassword = () => {
+        signInWithEmailAndPassword(getAuth(), email, password)
+    }
+
     return (
         <div className='p-4 flex flex-col items-center w-full'>
             <h1>Welcome to NojaNoja</h1>
             <br />
-            <br />
-            {/* <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> */}
+
             <button onClick={() => loginWithGoogle()} className='flex flex-row gap-2 items-center border-1 bg-yellow-200 p-2 rounded-md'>
                 <Image src={"/google-signin.png"} alt='Google Logo' width={32} height={32} />Sign in with Google
+            </button>
+
+            <button onClick={() => loginWithEmailPassword()} className='flex flex-row gap-2 items-center border-1 bg-yellow-200 p-2 rounded-md'>
+                <Image src={"/logo192.png"} alt='Email Icon' width={32} height={32} />Sign in with Email and Password
             </button>
         </div>
     )
